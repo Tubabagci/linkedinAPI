a9c80e97e2dc2ed18b93c467dea38310da0489e2
Tuğba Bağcı <57172097+Tubabagci@users.noreply.github.com>
2022-08-17 12:29:44 +0300

    oluşturma

---
+++ linkedinAPI/linkedinAPI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace linkedinAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Get the ClientID and ClientSecret to get the access token
+            string client_id = "*********Y77x1u7cb9kasbq";
+            string client_secret = "*******sPZtqbEfdsSlH0AX";
+            string organization_id = "********2928372";
+
+            string base_url = "https://www.linkedin.com/developers/apps/verification/3fd3b801-ea36-4483-ae83-734d8093e6c7";
+            string redirect_uri = "https://www.nuevo.com.tr/auth/linkedin/callback";
+            //scope = "w_member_social,r_liteprofile"
+
+            string url = "f" + base_url + "?response_type=code&client_id=" + client_id + "&state=random&redirect_uri=" + redirect_uri + "&scope={scope}";
+            Console.WriteLine(url);
+        }
+    }
+}
